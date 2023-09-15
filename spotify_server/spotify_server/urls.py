02d10782fe908b21156c95fe6902d5a2abacaaa9
Christian Abella <christian.abella38@gmail.com>
2023-09-14 22:39:42 -0700

    oops

--- spotify_server/spotify_server/urls.py
+++ spotify_server/spotify_server/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('spotify/', include('spotify_server.urls'))
+    path('spotify/', include('spotify_api.urls'))
 ]
